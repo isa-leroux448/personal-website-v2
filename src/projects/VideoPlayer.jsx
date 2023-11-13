1b124d6515b11b28a6938f4ac1d2d351c6e3de28
isa-leroux448 <isa.leroux448@gmail.com>
2023-11-12 22:08:25 -0700

    finish video player

--- src/projects/VideoPlayer.jsx
+++ src/projects/VideoPlayer.jsx
@@ -1,8 +1,12 @@
-import { useState, useCallback} from "react";
+import { useState, useCallback, useRef} from "react";
 import {motion, useDragControls} from 'framer-motion';
 import Header from "../components/Header";
 import ReactPlayer from 'react-player';
 import {projects} from './project-data';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const VideoPlayer = ({ currentZ, setCurrentZ }) => {
     const [localZ, setLocalZ] = useState(0);
@@ -12,22 +16,44 @@ const VideoPlayer = ({ currentZ, setCurrentZ }) => {
     }, []);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [currentProject, setCurrentProject] = useState(projects[0]);
-    const [loading, setLoading] = useState(false);
-  
-    const dragControls = useDragControls();
 
-    const selectProject = (index) => {
-      setLoading(true);
+    const dragControls = useDragControls();
+    
+    const swiperRef = useRef();
+
+    const selectProject = (newIndex) => {
+      const difference = newIndex - currentIndex;
+      if (difference > 0) {
+        for (let i = 0; i < difference; i++) {
+          swiperRef.current.slideNext();
+        }
+      }
+      if (difference < 0) {
+        for (let i = 0; i < difference*(-1); i++) {
+          swiperRef.current.slidePrev();
+        }
+      }
       currentProject.selected = false;
-      projects[index].selected = true;
-      setCurrentIndex(index);
-      setCurrentProject(projects[index]);
+      projects[newIndex].selected = true;
+      setCurrentIndex(newIndex);
+      setCurrentProject(projects[newIndex]);
     }
 
-    const handlePlayerReady = () => {
-      console.log("hello");
-      setLoading(false);
-    };
+    const swipeNext = () => {
+      const newIndex = currentIndex+1;
+      currentProject.selected = false;
+      projects[newIndex].selected = true;
+      setCurrentIndex(newIndex);
+      setCurrentProject(projects[newIndex]);
+    }
+
+    const swipeBack = () => {
+      const newIndex = currentIndex-1;
+      currentProject.selected = false;
+      projects[newIndex].selected = true;
+      setCurrentIndex(newIndex);
+      setCurrentProject(projects[newIndex]);
+    }
   
     return (
       open && (
@@ -51,12 +77,34 @@ const VideoPlayer = ({ currentZ, setCurrentZ }) => {
           <div style={{width: '100%', display: "flex", flexGrow: 1}}>
           <div style={{width: '75%', display: "flex", flexDirection: "column"}}>
               <div style={{backgroundColor: '#121619', display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '65%', position: 'relative'}}>
-                  {loading && <img style={{maxHeight: '25%', position: 'absolute', top: '40%', left: '45%'}} src={require('../assets/loading.gif')} alt="Loading" />}
-                  <ReactPlayer url={currentProject?.videoLink} width='100%' height='100%' controls={true} onReady={handlePlayerReady} style={{visibility: loading ? 'hidden':'visible'}}/>
+                  <Swiper
+                  navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                  }}
+                  modules={[Navigation]}
+                  style={{ height: '100%' }}
+                  onSwiper={(swiper) => {
+                    swiperRef.current = swiper;
+                  }}
+                >
+                  {projects.map((project, index) => (
+                    <SwiperSlide key={index}>
+                      <ReactPlayer
+                        url={project?.videoLink}
+                        width='100%'
+                        height='100%'
+                        controls={true}
+                      />
+                    </SwiperSlide>
+                  ))}
+                  <div className="swiper-button-next" onClick={() => swipeNext()} style={{ color: '#ECECEC' }}></div>
+                  <div className="swiper-button-prev" onClick={() => swipeBack()} style={{ color: '#ECECEC' }}></div>
+                </Swiper>
                 </div>
                 <div style={{backgroundColor: 'white', display: "flex", flex: 1, flexDirection: "column"}}>
                 {projects.map((project, index) => (
-                <div onClick={() => selectProject(index)} key={index} style={{padding: '3px', paddingLeft: '10px', border: '0.5px solid black', backgroundColor: project.selected ? '#FF8B8B': 'white'}}>{project.title}</div>))}
+                <div onClick={() => selectProject(index)} key={index} style={{padding: '3px', paddingLeft: '10px', border: '0.5px solid black', backgroundColor: project.selected ? '#FF8B8B': 'white', cursor: 'pointer'}}>{project.title}</div>))}
                 </div>
             </div>
             <div style={{backgroundColor: 'white', width: '25%'}}>
