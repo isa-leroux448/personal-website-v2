e86fbf4a9f1b76aaf4fe8bb3b71f53f5b73348c9
isa-leroux448 <isa.leroux448@gmail.com>
2023-12-24 21:40:51 -0800

    add dropdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import '../styles.css';
 import logo from '../assets/logo.png'
+import { useState } from "react";
 
 const NavBar = ({navigateToSlide}) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowDropdown(true);
+    }
+
+    const handleMouseLeave = () => {
+        setShowDropdown(false);
+    }
+
     return (
         <div className="sticky">
             <header className="navBarBg">
@@ -14,6 +25,23 @@ const NavBar = ({navigateToSlide}) => {
                     <button className="navBarItem" onClick={() => navigateToSlide(1)}>Experience</button>
                     <button className="navBarItem" onClick={() => navigateToSlide(2)}>Skills</button>
                     <button className="navBarItem" onClick={() => navigateToSlide(3)}>Projects</button>
+                    <div style={{display: 'inline-block', position: 'relative'}} onMouseLeave={handleMouseLeave}>
+                        <button className="navBarItem" style={{width: '120px'}} onMouseEnter={handleMouseEnter}>Contact</button>
+                        {showDropdown && (<div style={{position: 'absolute'}}>
+                            <a href="https://www.linkedin.com/in/isabella-leroux/" target="_blank">
+                                <button className="dropdownItem">LinkedIn</button>
+                            </a>
+                            <a href="" target="_blank">
+                                <button className="dropdownItem">Resume</button>
+                            </a>
+                            <a href="https://github.com/isa-leroux448" target="_blank">
+                                <button className="dropdownItem">GitHub</button>
+                            </a>
+                            <a href="mailto:isabella_leroux@yahoo.ca">
+                                <button className="dropdownItem">Email</button>
+                            </a>
+                        </div>)}
+                    </div>
                 </nav>
             </header>
             <div className="line" />
